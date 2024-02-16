56c83dbb22d35c64f650be445dea248c99e3d786
MItrovic zeljko <zeljko.mitrovic@ynov.com>
2024-02-16 11:11:29 +0100

    finished for abdel the king of algerian

--- src/handlers.go
+++ src/handlers.go
@@ -20,6 +20,32 @@ func FirstHistory(w http.ResponseWriter, r *http.Request) {
 func Character2(w http.ResponseWriter, r *http.Request) {
 	RenderTemplate(w, "character2")
 }
+func Character1(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "character1")
+}
+func Character15(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "character15")
+}
+func IntroHangman(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "introhangman")
+}
+func FirstHangman(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "firsthangman")
+}
+func EndFirstHangman(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "endfirsthangman")
+}
+func EndOfChapter1(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "endofchapter1",)
+}
+func Image1to2(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "image1to2",)
+}
+func Image0to1(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "image0to1",)
+}
+
+
 
 
 func RenderTemplate(w http.ResponseWriter, html string) {
--- src/main.go
+++ src/main.go
@@ -8,16 +8,23 @@ import (
 const port = ":8080"
 
 func main() {
-	
+	http.Handle("/templates_HTML/", http.StripPrefix("/templates_HTML/", http.FileServer(http.Dir("templates_HTML"))))
+
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/startgame",Startgame)
 	http.HandleFunc("/character",Character)
 	http.HandleFunc("/firsthistory",FirstHistory)
 	http.HandleFunc("/character2",Character2)
-	fs := http.FileServer(http.Dir("templates_HTML"))
-	http.Handle("/templates_HTML", http.StripPrefix("/templates_HTML/", fs))
+	http.HandleFunc("/character1",Character1)
+	http.HandleFunc("/character15",Character15)
+	http.HandleFunc("/introhangman",IntroHangman)
+	http.HandleFunc("/firsthangman",FirstHangman)
+	http.HandleFunc("/endfirsthangman",EndFirstHangman)
+	http.HandleFunc("/endofchapter1",EndOfChapter1)
+	http.HandleFunc("/image1to2",Image1to2)
+	http.HandleFunc("/image0to1",Image0to1)
 
-	fmt.Println("(htpp://localhost:8080) - Server started on port ", port)
+	fmt.Println("(http://localhost:8080) - Server started on port ", port)
 	http.ListenAndServe(port, nil)
 }
 
